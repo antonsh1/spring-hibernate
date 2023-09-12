6a5265861e18273f681fdac5a170ed6ee9e82e82
Anton Shelepov <antonsh1@mail.ru>
2023-09-12 16:08:56 +0500

    Выполнено задание Задача «Безопасное приложение»

--- README.md
+++ README.md
@@ -1,14 +1,19 @@
-### Домашнее задание к занятию «8.5. Spring Data JPA»
+### Домашнее задание к занятию «8.6. Безопасность в веб-приложениях, Spring Security»
 
-# Задача «Слой DAO c JPA Repositories»
+# Задача «Безопасное приложение»
 
-[Ссылка на задание](https://github.com/netology-code/jd-homeworks/blob/master/spring-jpa/task1/README.md)
+[Ссылка на задание](https://github.com/netology-code/jd-homeworks/blob/master/spring_security/task1/README.md)
 
+### Пользователи системы
+* user1
+* user2
+* admin
+Пароль у всех password
 
-### Ссылки для получения корректных данных:
-* По городу
+### Ссылки для получения данных:
+* Доступен всем
 http://localhost:8080/persons/by-city?city=Ekaterinburg
-* По возрасту
+* Доступен user2 или admin
 http://localhost:8080/persons/by-age-less?age=15
-* По Имени и Фамилии
+* Доступен admin
 http://localhost:8080/persons/by-name-surename?name=Alexey&sureName=Ivanov
---
+++ src/main/java/ru/smartjava/springhibernate/security/WebSecurityConfig.java
@@ -0,0 +1,58 @@
+package ru.smartjava.springhibernate.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user1 = User.withUsername("user1")
+                .password(passwordEncoder().encode("password"))
+                .roles("CITY")
+                .build();
+        UserDetails user2 = User.withUsername("user2")
+                .password(passwordEncoder().encode("password"))
+                .roles("AGE")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("password"))
+                .roles("ALL")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2, admin);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/persons/by-city").permitAll()
+                        .requestMatchers("/persons/by-age-less").hasAnyRole("AGE","ALL")
+                        .requestMatchers("/persons/by-name-surename").hasRole("ALL")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(withDefaults()
+                )
+                .logout(LogoutConfigurer::permitAll);
+
+        return http.build();
+    }
+
+}
